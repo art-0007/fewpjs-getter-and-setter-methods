24df9d87458536b229bdba426f46b64a0a500026
Artur <info@aigroupusa.com>
2022-01-05 13:51:26 -0500

    commit tree 43351083b92c21ef4469198d50ffc5e68b8847ca

--- index.js
+++ index.js
@@ -1,1 +1,30 @@
 // Add your Circle class here
+class Circle {
+    constructor (radius) {
+        this.radius = radius
+    }
+    
+    get diameter() {
+        return this.radius * 2;
+    }
+
+    get circumference() {
+        return Math.PI * this.radius * 2;
+    }
+
+    get area() {
+        return Math.PI * this.radius**2;
+    }
+
+    set diameter(newDiametr) {
+        this.radius = newDiametr/2;
+      }
+
+    set circumference(newcirCumference) {
+        this.radius = (newcirCumference/Math.PI)/2;
+      }
+
+    set area(newArea) {
+        this.radius = Math.sqrt(newArea/Math.PI);
+      }
+}
